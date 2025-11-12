ef35a6190236cdbb4e67ffc32b7cad50ffae64c5
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <aobermayer4@gmail.com>
2025-11-12 13:07:54 -0500

    add config file argument, allow single column inputs

--- lib/junctionScope.sh
+++ lib/junctionScope.sh
@@ -3,19 +3,26 @@
 # Example Use
 # sh junctionScope.sh -i sample_inputfiles.txt -r chr1:1000-2000 -n ATCGGACATT -f ref.fa -g ref.gtf -o junctionScope_output -b 200 -s 0 -t 8
 
-usage() { echo "Usage: $0 [-i <list file>] [-r <chr1:1000-2000>] [-n <nucleotide sequence>] [-f <FASTA reference>] [-g <GTF reference>] [-o <output directory>] [-b <int nucleotide region buffer>] [-s <0|1|2>] [-t <int threads>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-c <config file>] [-i <list file>] [-r <chr1:1000-2000>] [-n <nucleotide sequence>] [-f <FASTA reference>] [-g <GTF reference>] [-o <output directory>] [-b <int nucleotide region buffer>] [-s <0|1|2>] [-t <int threads>]" 1>&2; exit 1; }
 
 # built with
 # ml SAMtools/1.9-foss-2018b
 # ml RegTools/0.5.2-foss-2021b
+# ml OpenBLAS/0.3.18-GCC-11.2.0
 
 # Defaults
 BUFFER=200
 THREADS=1
 STRAND=0
 
-while getopts ":i:r:n:f:g:o:b:s:t:" opt; do
+# region needs to be adjusted if chr or no chr in files
+# fasta index should be in folder with fasta, regtools might build onthe fly though
+
+while getopts ":c:i:r:n:f:g:o:b:s:t:" opt; do
   case ${opt} in
+  c)
+  	CONFIG=${OPTARG}
+  	;;
   i)
   	INPUT=${OPTARG}
   	;;
@@ -47,12 +54,17 @@ while getopts ":i:r:n:f:g:o:b:s:t:" opt; do
       echo "Invalid option: -$OPTARG" >&2
       exit 1
       ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
+    #:)
+    #  echo "Option -$OPTARG requires an argument." >&2
+    #  exit 1
+    #  ;;
   esac
 done
+
+if [ -f ${CONFIG} ]; then
+	source ./${CONFIG}
+fi
+
 
 if [ -z "${INPUT}" ] || [ -z "${REGION}" ] || [ -z "${NTSEQ}" ] || [ -z "${FASTA}" ] || [ -z "${GTF}" ]; then
     usage
@@ -64,21 +76,37 @@ mkdir -p ${OUTPUT}/extractJxnSequence
 mkdir -p ${OUTPUT}/extractJxnRegtools
 mkdir -p ${OUTPUT}/annotateJxnRegtools
 
-while IFS=$'\t' read -r sample file; do
-	file="${file//[$'\r\n']/}"
-	# setup
-	# samtools extract
-	echo "sh extractJxnRegion.sh -i ${file} -r ${REGION} -o ${OUTPUT}/extractJxnRegion/${sample}.region.sam -b ${BUFFER} -t ${THREADS}" >> ${OUTPUT}/extractJxnRegion_batch.sh
-	echo "sh extractJxnSequence.sh -i ${OUTPUT}/extractJxnRegion/${sample}.region.sam -n ${NTSEQ} -o ${OUTPUT}/extractJxnSequence/${sample}.region.seq.sam" >> ${OUTPUT}/extractJxnSequence_batch.sh
-	# regtools extract
-	echo "sh extractJxnRegtools.sh -i ${file} -r ${REGION} -s ${STRAND} -o ${OUTPUT}/extractJxnRegtools/${sample}.region.bed" >> ${OUTPUT}/extractJxnRegtools_batch.sh
-	echo "sh annotateJxnRegtools.sh -i ${OUTPUT}/extractJxnRegtools/${sample}.region.bed -f ${FASTA} -g ${GTF} -o ${OUTPUT}/annotateJxnRegtools/${sample}.region.bed.anno" >> ${OUTPUT}/annotateJxnRegtools_batch.sh
-done < ${INPUT}
+inFileCols=$(awk -F'\t' '{print NF; exit}' ${INPUT})
 
-echo "sh ${OUTPUT}/extractJxnRegion_batch.sh" >> junctionScope_exc.sh
-echo "sh ${OUTPUT}/extractJxnSequence_batch.sh" >> junctionScope_exc.sh
-echo "sh ${OUTPUT}/extractJxnRegtools_batch.sh" >> junctionScope_exc.sh
-echo "sh ${OUTPUT}/annotateJxnRegtools_batch.sh" >> junctionScope_exc.sh
-echo "sh countJxnSequence.sh -d ${OUTPUT}/extractJxnSequence/ -o ${OUTPUT}/output.JxnCount.txt" >> junctionScope_exc.sh
-echo "sh matchJxnRegtools.sh -d ${OUTPUT}/annotateJxnRegtools/ -r ${REGION} -o ${OUTPUT}/outputJxnCount.regtools.txt" >> junctionScope_exc.sh
+if [[ ${inFileCols} -eq 1 ]]; then
+	while IFS=$'\t' read -r file; do
+		# setup
+		file="${file//[$'\r\n']/}"
+		sample=$(basename ${file})
+		# samtools extract
+		echo "sh extractJxnRegion.sh -i ${file} -r ${REGION} -o ${OUTPUT}/extractJxnRegion/${sample}.region.sam -b ${BUFFER} -t ${THREADS}" >> ${OUTPUT}/extractJxnRegion_batch.sh
+		echo "sh extractJxnSequence.sh -i ${OUTPUT}/extractJxnRegion/${sample}.region.sam -n ${NTSEQ} -o ${OUTPUT}/extractJxnSequence/${sample}.region.seq.sam" >> ${OUTPUT}/extractJxnSequence_batch.sh
+		# regtools extract
+		echo "sh extractJxnRegtools.sh -i ${file} -r ${REGION} -s ${STRAND} -o ${OUTPUT}/extractJxnRegtools/${sample}.region.bed" >> ${OUTPUT}/extractJxnRegtools_batch.sh
+		echo "sh annotateJxnRegtools.sh -i ${OUTPUT}/extractJxnRegtools/${sample}.region.bed -f ${FASTA} -g ${GTF} -o ${OUTPUT}/annotateJxnRegtools/${sample}.region.bed.anno" >> ${OUTPUT}/annotateJxnRegtools_batch.sh
+	done < ${INPUT}
+else
+	while IFS=$'\t' read -r sample file; do
+		# setup
+		file="${file//[$'\r\n']/}"
+		# samtools extract
+		echo "sh extractJxnRegion.sh -i ${file} -r ${REGION} -o ${OUTPUT}/extractJxnRegion/${sample}.region.sam -b ${BUFFER} -t ${THREADS}" >> ${OUTPUT}/extractJxnRegion_batch.sh
+		echo "sh extractJxnSequence.sh -i ${OUTPUT}/extractJxnRegion/${sample}.region.sam -n ${NTSEQ} -o ${OUTPUT}/extractJxnSequence/${sample}.region.seq.sam" >> ${OUTPUT}/extractJxnSequence_batch.sh
+		# regtools extract
+		echo "sh extractJxnRegtools.sh -i ${file} -r ${REGION} -s ${STRAND} -o ${OUTPUT}/extractJxnRegtools/${sample}.region.bed" >> ${OUTPUT}/extractJxnRegtools_batch.sh
+		echo "sh annotateJxnRegtools.sh -i ${OUTPUT}/extractJxnRegtools/${sample}.region.bed -f ${FASTA} -g ${GTF} -o ${OUTPUT}/annotateJxnRegtools/${sample}.region.bed.anno" >> ${OUTPUT}/annotateJxnRegtools_batch.sh
+	done < ${INPUT}
+fi
+
+echo "sh ${OUTPUT}/extractJxnRegion_batch.sh" >> ${OUTPUT}_exc.sh
+echo "sh ${OUTPUT}/extractJxnSequence_batch.sh" >> ${OUTPUT}_exc.sh
+echo "sh ${OUTPUT}/extractJxnRegtools_batch.sh" >> ${OUTPUT}_exc.sh
+echo "sh ${OUTPUT}/annotateJxnRegtools_batch.sh" >> ${OUTPUT}_exc.sh
+echo "sh countJxnSequence.sh -d ${OUTPUT}/extractJxnSequence/ -o ${OUTPUT}/output.JxnCount.txt" >> ${OUTPUT}_exc.sh
+echo "sh matchJxnRegtools.sh -d ${OUTPUT}/annotateJxnRegtools/ -r ${REGION} -o ${OUTPUT}/outputJxnCount.regtools.txt" >> ${OUTPUT}_exc.sh
 
